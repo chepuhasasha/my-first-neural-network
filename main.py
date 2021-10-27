df00046aa1911864025d3646faeac9eb597f1548
Sergey Batukov <sashachepuha@gmail.com>
2021-10-27 12:25:03 +0000

    init function

---
+++ main.py
@@ -0,0 +1,16 @@
+class neuralNetwark:
+  def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
+    # задание количества узлов на разных слоях
+    self.i_nodes = input_nodes
+    self.h_nodes = hidden_nodes
+    self.o_nodes = output_nodes
+
+    # коэффициент обучения
+    self.lr = learning_rate
+    pass
+  def train():
+    pass
+  def query():
+    pass
+
+n = neuralNetwark(3, 3, 3, 0.3)
