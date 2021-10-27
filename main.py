280e658b217de0f97c43ebb7e7c1f733d046a211
Sergey Batukov <sashachepuha@gmail.com>
2021-10-27 12:38:04 +0000

    activation function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy
+import scipy.special
 
 class neuralNetwark:
   def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
@@ -10,9 +11,12 @@ class neuralNetwark:
     # коэффициент обучения
     self.lr = learning_rate
 
+    # матрицы весовых коэффициентов
     self.wih = numpy.random.normal(0.0, pow(self.h_nodes, -0.5), (self.h_nodes, self.i_nodes))
     self.who = numpy.random.normal(0.0, pow(self.o_nodes, -0.5), (self.o_nodes, self.h_nodes))
-    print(self.who)
+
+    # использование сигмойды в качестве функции активации
+    self.activation_func = lambda x: scipy.special.expit(x)
     pass
   def train():
     pass
