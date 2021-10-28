7304342748ef3d3e38a4c4fea027ed1885ef2107
sashachepuha <sashachepuha@gmail.com>
2021-10-28 10:23:40 +0300

    method train

--- main.py
+++ main.py
@@ -19,8 +19,37 @@ class neuralNetwark:
     self.activation_func = lambda x: scipy.special.expit(x)
 
     pass
-  def train():
+
+  def train(self, inputs_list, target_list):
+    # преобразовать список входных значений в двумерный массив
+    inputs = numpy.array(inputs_list, ndmin=2).T
+    targets = numpy.array(target_list, ndmin=2).T
+
+    # расчет входяших сигналов для скрытого слоя
+    h_inputs = numpy.dot(self.who, inputs)
+    # расчет исходяших сигналов для скрытого слоя
+    h_outputs = self.activation_func(h_inputs)
+
+    # расчет входяших сигналов для выходного слоя
+    final_inputs = numpy.dot(self.who, h_outputs)
+    # расчет исходяших сигналов для выходного слоя
+    final_outputs = self.activation_func(final_inputs)
+
+    # ошибки выходного слоя = (целевое - фактическое)
+    output_errors = targets - final_outputs
+    # ошибки скрытого слоя - это outputs_errors, 
+    # распределенные прпорционально весовым коэфф. связей
+    # и рекомбенированные на скрытых узлах
+    h_errors = numpy.dot(self.who.T, output_errors)
+
+    # обновление весовых коэфф. для связей между входными и скрытыми слоями
+    self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)), numpy.transpose(h_outputs))
+
+    # обновление весовых коэфф. для связей между входными и скрытыми слоями
+    self.wih += self.lr * numpy.dot((h_errors * h_outputs * (1.0 - h_outputs)), numpy.transpose(inputs))
+   
     pass
+
   def query(self, inputs_list):
     # преобразование списка входных значений в двумерный массив
     inputs = numpy.array(inputs_list, ndmin=2).T
