9c1bf86f553caa1f43ecb63564366866a2c7ee7e
sashachepuha <sashachepuha@gmail.com>
2021-11-02 11:11:52 +0300

    remove PIL

--- new.py
+++ new.py
@@ -1,11 +1,5 @@
 import numpy
 import scipy.special
-from PIL import Image, ImageDraw
- 
-# Пустой желтый фон.
-im = Image.new('RGB', (500, 300), (219, 193, 27))
-draw = ImageDraw.Draw(im)
-im.show()
 
 
 class NN:
@@ -13,53 +7,55 @@ class NN:
         self.actvation = lambda x: scipy.special.expit(x)  # сигмойда
         self.lr = lr  # коэф. обучения
         self.network = []
-        
+
         for i, item in enumerate(config):  # создание слоев
-            lvl = { 'id': i, 'in': [], 'out': [], 'err':[] }
+            lvl = {'id': i, 'in': [], 'out': [], 'err': []}
             if (i + 1) < len(config):
                 lvl['w'] = numpy.random.normal(
                     0.0, pow(config[i + 1], -0.5), (config[i + 1], item))
             self.network.append(lvl)
         pass
 
-    def query(self, input): # опрос сети
+    def query(self, input):  # опрос сети
         for i, lvl in enumerate(self.network):
-            if i == 0: #входной слой
+            if i == 0:  # входной слой
                 in_lvl = numpy.array(input, ndmin=2).T
                 lvl['in'] = in_lvl
                 lvl['out'] = in_lvl
             else:
-                in_lvl = numpy.dot(self.network[i-1]['w'], self.network[i-1]['out'])
+                in_lvl = numpy.dot(
+                    self.network[i-1]['w'], self.network[i-1]['out'])
                 lvl['in'] = in_lvl
                 lvl['out'] = self.actvation(in_lvl)
         return self.network
 
-    def update(self, error, out, out_prev): # дефференцированая функция градиентного спуска
+    def update(self, error, out, out_prev):  # дефференцированая функция градиентного спуска
         return self.lr * numpy.dot((e * out * (1.0 - out)), numpy.transpose(out_prev))
 
     def train(self, input, target):
-        self.query(input) # состояние сети
-        t = numpy.array(target, ndmin=2).T, # приведение к двумерному массиву
-        out = self.network[-1]['out'] # выход с последнего слоя
+        self.query(input)  # состояние сети
+        t = numpy.array(target, ndmin=2).T,  # приведение к двумерному массиву
+        out = self.network[-1]['out']  # выход с последнего слоя
         # обратное распростронение ошибки
         for i, lvl in reversed(list(enumerate(self.network))):
             if i == (len(self.network) - 1):
-                lvl['err'] = pow(((t - out)[0]), 2) # функция выходной ошибки
+                lvl['err'] = pow(((t - out)[0]), 2)  # функция выходной ошибки
             else:
                 lvl['err'] = numpy.dot(lvl['w'].T, self.network[i+1]['err'])
-        self.show()
         pass
 
     def show(self):
-        print('- NETWORK -')  
+        print('- NETWORK -')
         for lvl in self.network:
             print('lavel:', lvl['id'])
             print('|||||||||||||||||||||||||||||||||||||||||||||||||||')
             print('in: \n', lvl['in'])
             print('out: \n', lvl['out'])
             if 'w' in lvl:
-              print('w: \n', lvl['w'])
+                print('w: \n', lvl['w'])
             print('err: \n', lvl['err'])
 
+
 n = NN(0.3)
-n.train([10,10,10], [1,0.5,0.5])
+n.train([10, 10, 10], [1, 0.5, 0.5])
+n.show()
